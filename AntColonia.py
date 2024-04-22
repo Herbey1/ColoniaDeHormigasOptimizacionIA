0bfe9df34e606e31deded94c30690c0954eced86
Gilberto <jose.tellez38@uabc.edu.mx>
2024-04-21 23:55:03 -0700

    Le agregue las ciudades indicadas , y que pueda elegir la ciudad que quiere (inicial y destino)

--- AntColonia.py
+++ AntColonia.py
@@ -24,26 +24,24 @@ class AntColony(object):
         #Despues, dividimos la matriz de 1, entre el numero de ciudades (len(distances)), para obtener una matriz de feromonas inicial.
         self.all_indices = range(len(distancias))
 
-    def correr(self):
+    def correr(self, inicio, destino):
         cortoiteracion = None
         elmascorto = ("unodostres", np.inf)
 
         for i in range(self.n_veces):
-            #Generamos todas las rutas posibles
-            todoscaminos = self.hacertodoscaminos()
-            #Esparcimos feromonas en las n_best rutas, si es que son mejores que la mejor ruta actual.
+            # Generamos todas las rutas posibles
+            todoscaminos = self.hacertodoscaminos(inicio, destino)
+            # Esparcimos feromonas en las n_best rutas, si es que son mejores que la mejor ruta actual.
             self.tirarferomona(todoscaminos, self.n_best, cortoiteracion=cortoiteracion)
-            #Actualizamos la mejor ruta
+            # Actualizamos la mejor ruta
             cortoiteracion = min(todoscaminos, key=lambda x: x[1])
-            #Imprimimos la mejor ruta
-            print (cortoiteracion)
-            #Si la mejor ruta actual es mejor que la mejor ruta de todos los tiempos.
+            # Si la mejor ruta actual es mejor que la mejor ruta de todos los tiempos.
             if cortoiteracion[1] < elmascorto[1]:
-                #Actualizamos la mejor ruta de todos los tiempos.
+                # Actualizamos la mejor ruta de todos los tiempos.
                 elmascorto = cortoiteracion    
-            #Decrecemos la feromona en todas las rutas        
+            # Decrecemos la feromona en todas las rutas        
             self.feromona = self.feromona * self.decay    
-        #Regresamos la mejor ruta de todos los tiempos.        
+        # Regresamos la mejor ruta de todos los tiempos.        
         return elmascorto
     
     #Función para esparcir feromonas en las n_best rutas.
@@ -63,25 +61,28 @@ class AntColony(object):
         return total_dist
 # Distancia total de una ruta. Una ruta "path" de entrada, itera sobre cada elemento de la ruta, entra a la matriz de distancias para obtener la distancia correspondiente a cada par de ciudades consecutivas en la ruta, y suma todas las distancias para obtener la distancia total de la ruta, devolviendo la distancia total calculada.
 
-    def hacertodoscaminos(self):
+    def hacertodoscaminos(self, inicio, destino):
         todoscaminos = []
         for i in range(self.n_ants):
-            camino = self.generarcamino(0)
+            camino = self.generarcamino(inicio, destino)
             todoscaminos.append((camino, self.distanciatotal(camino)))
         return todoscaminos
 # Posibles rutas. Genera una lista vacia que almacena las rutas generadas. Itera sobre self.n_ants, y para cada iteracion se genera una ruta para una hormiga y calcula la distancia total de esa ruta usando el metodo anterior. Y agregamos la ruta con la distancia a la matriz generada. Y devuelve todas al final.
 
-    def generarcamino(self, inicio):
+    def generarcamino(self, inicio, destino):
         camino = []
         visito = set()
         visito.add(inicio)
         prev = inicio
         for i in range(len(self.distancias) - 1):
             moverse = self.movimiento(self.feromona[prev], self.distancias[prev], visito)
+            if moverse is None:
+                # No hay movimientos válidos disponibles, romper el ciclo
+                break
             camino.append((prev, moverse))
             prev = moverse
             visito.add(moverse)
-        camino.append((prev, inicio))
+        camino.append((prev, destino))
         return camino
 # Genera una ruta para una hormiga. Se inicializa una lista vacia "path" que almacenara la ruta generada. Se inicializa un conjunto vacio "visited" que almacenara las ciudades visitadas. Se agrega la ciudad inicial a "visited". Se inicializa la variable "prev" con la ciudad inicial. Se itera sobre el rango de la longitud de la matriz de distancias menos 1. Se elige un movimiento con el metodo "pick_move" que se explicara despues. Se agrega el movimiento a la ruta. Se actualiza la ciudad previa a la ciudad actual. Se agrega la ciudad actual a las ciudades visitadas. Al final se agrega el movimiento de regreso a la ciudad inicial. Y se devuelve la ruta generada.
 
@@ -89,9 +90,16 @@ class AntColony(object):
         feromona = np.copy(feromona)
         feromona[list(visito)] = 0
 
-        fila = feromona ** self.alpha * (( 1.0 / dist) ** self.beta)
+        fila = feromona ** self.alpha * ((1.0 / dist) ** self.beta)
+        fila[np.isinf(dist)] = 0  # Establecer probabilidad a cero para distancias infinitas
 
-        norm_fila = fila / fila.sum()
-        moverse = np_choice(self.all_indices, 1, p=norm_fila)[0]
+        indices_validos = np.flatnonzero(fila)
+        if indices_validos.size > 0:
+            norm_fila = fila[indices_validos] / fila[indices_validos].sum()
+            moverse = np_choice(indices_validos, 1, p=norm_fila)[0]
+        else:
+            # No hay movimientos válidos disponibles
+            moverse = None
+
         return moverse
 # Elige un movimiento para una hormiga. Se copia la matriz de feromonas. Se establece a 0 las feromonas de las ciudades visitadas. Se calcula la probabilidad de moverse a una ciudad con la formula de seleccion de la proxima ciudad. Se normaliza la fila de probabilidades. Se elige un movimiento con la funcion np_choice. Y se devuelve el movimiento elegido.
